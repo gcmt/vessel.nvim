884740a1f143ed8c351b8aa84851b097b3c21dc1
Giacomo Comitti <dev@gcomit.com>
2024-09-26 19:06:53 +0200

    refactor: remove reduntant nil checks
    Nil checking is already handled by the config api

--- lua/vessel.lua
+++ lua/vessel.lua
@@ -7,7 +7,7 @@ local M = {}
 ---@param opts table?  Config table
 ---@return boolean If the mark has been successfully set
 local function set_mark(global, opts)
-	local config = require("vessel.config").get(opts or {})
+	local config = require("vessel.config").get(opts)
 	local app = require("vessel.core"):new(config)
 	local marklist = require("vessel.marklist"):new(app):init()
 	return marklist:set_mark(global)
@@ -28,7 +28,7 @@ function M.set_global_mark(opts)
 end
 
 --- Open the mark list window
----@param opts table Config overrides
+---@param opts table? Config overrides
 ---@param filter_func function?
 function M.view_marks(opts, filter_func)
 	local config = require("vessel.config").get(opts)
@@ -40,15 +40,16 @@ end
 --- Open the mark list window with only entries belonging to the current buffer
 ---@param opts table? Config overrides
 function M.view_local_marks(opts)
-	M.view_marks(opts or {}, function(mark, context)
+	M.view_marks(opts, function(mark, context)
 		return mark.file == context.bufpath
 	end)
 end
 
 --- Open the jump list window
 ---@param opts table? Config overrides
+---@param filter_func function?
 function M.view_jumps(opts, filter_func)
-	local config = require("vessel.config").get(opts or {})
+	local config = require("vessel.config").get(opts)
 	local app = require("vessel.core"):new(config)
 	local jumplist = require("vessel.jumplist"):new(app, filter_func)
 	jumplist:open()
@@ -57,7 +58,7 @@ end
 --- Open the jump list window with only entries belonging to the current buffer
 ---@param opts table? Config overrides
 function M.view_local_jumps(opts)
-	M.view_jumps(opts or {}, function(jump, context)
+	M.view_jumps(opts, function(jump, context)
 		return jump.bufnr == context.bufnr
 	end)
 end
@@ -70,46 +71,46 @@ function M.setup(opts)
 
 	if config.marks.create_commands then
 		vim.api.nvim_create_user_command(config.marks.commands.view, function(_)
-			M.view_marks({})
+			M.view_marks()
 		end, { nargs = 0 })
 		vim.api.nvim_create_user_command(config.marks.commands.set_local, function(_)
-			M.set_local_mark({})
+			M.set_local_mark()
 		end, { nargs = 0 })
 		vim.api.nvim_create_user_command(config.marks.commands.set_global, function(_)
-			M.set_global_mark({})
+			M.set_global_mark()
 		end, { nargs = 0 })
 	end
 
 	if config.jumps.create_commands then
 		vim.api.nvim_create_user_command(config.jumps.commands.view, function(_)
-			M.view_jumps({})
+			M.view_jumps()
 		end, { nargs = 0 })
 	end
 end
 
 vim.keymap.set("n", "<plug>(VesselViewMarks)", function()
-	M.view_marks({})
+	M.view_marks()
 end)
 
 vim.keymap.set("n", "<plug>(VesselViewLocalMarks)", function()
-	M.view_local_marks({})
+	M.view_local_marks()
 end)
 
 vim.keymap.set("n", "<plug>(VesselSetLocalMark)", function()
-	M.set_local_mark({})
+	M.set_local_mark()
 end)
 
 vim.keymap.set("n", "<plug>(VesselSetGlobalMark)", function()
-	M.set_global_mark({})
+	M.set_global_mark()
 end)
 
 vim.keymap.set("n", "<plug>(VesselViewJumps)", function()
-	M.view_jumps({})
+	M.view_jumps()
 end)
 
 --- Filter out jumps not belonging to the current buffer
 vim.keymap.set("n", "<plug>(VesselViewLocalJumps)", function()
-	M.view_local_jumps({})
+	M.view_local_jumps()
 end)
 
 return M
