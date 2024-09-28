2132c051d402ca939776f290ee91168eb3cd4448
Giacomo Comitti <dev@gcomit.com>
2024-09-28 07:06:47 +0200

    refactor: move mappings creation to plugin directory

---
+++ plugin/vessel.lua
@@ -0,0 +1,40 @@
+if vim.g.loaded_vessel == 1 then
+  return
+end
+vim.g.loaded_vessel = 1
+
+vim.keymap.set("n", "<plug>(VesselViewMarks)", function()
+	require("vessel").view_marks()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewLocalMarks)", function()
+	require("vessel").view_local_marks()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewGlobalMarks)", function()
+	require("vessel").view_global_marks()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewExternalMarks)", function()
+	require("vessel").view_external_marks()
+end)
+
+vim.keymap.set("n", "<plug>(VesselSetLocalMark)", function()
+	require("vessel").set_local_mark()
+end)
+
+vim.keymap.set("n", "<plug>(VesselSetGlobalMark)", function()
+	require("vessel").set_global_mark()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewJumps)", function()
+	require("vessel").view_jumps()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewLocalJumps)", function()
+	require("vessel").view_local_jumps()
+end)
+
+vim.keymap.set("n", "<plug>(VesselViewExternalJumps)", function()
+	require("vessel").view_external_jumps()
+end)
